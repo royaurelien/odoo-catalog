f597cb5a7c66e169d018ec2d5e82fa03b19ca63d
Aurelien ROY <roy.aurelien@gmail.com>
2022-03-08 23:52:23 +0100

    [FIX] Git rules

--- custom_addons/models/git_sync.py
+++ custom_addons/models/git_sync.py
@@ -2,6 +2,7 @@
 
 from multiprocessing import synchronize
 from odoo import models, fields, api, _
+from odoo.tools import safe_eval
 
 from datetime import datetime
 import logging
@@ -47,6 +48,12 @@ class GitSync(models.AbstractModel):
             return x
         return list(map(apply, vals_list))
 
+    def _check_major_version(self, regex=REGEX_MAJOR_VERSION, vals_list):
+        def apply(x):
+            x['major'] = bool(regex.match(x.get('name', False)))
+            return x
+        return list(map(apply, vals_list))
+
     def _get_service(self, name):
         pass
 
@@ -77,41 +84,79 @@ class GitSync(models.AbstractModel):
     def _get_excludes(self):
         return [name.strip() for name in self.exclude_names.split(",") if name] if self.exclude_names else []
 
+    def _get_eval_context(self):
+        """ Prepare the context used when evaluating python code
+            :returns: dict -- evaluation context given to safe_eval
+        """
+        return {
+            'datetime': safe_eval.datetime,
+            'dateutil': safe_eval.dateutil,
+            'time': safe_eval.time,
+            'uid': self.env.uid,
+            'user': self.env.user,
+            'env': self.env,
+        }
+
+    def _apply_rule(self, rule, vals_list, context={}):
+        def ignore(vals):
+            _logger.error(vals)
+            try:
+                if not eval(rule.condition, context, vals):
+                    return vals
+                else:
+                    _logger.warning('Ignore {}'.format(vals.get('name')))
+            except ValueError as error:
+                _logger.error(error)
+
+        def apply(vals):
+            _logger.error(vals)
+            try:
+                if eval(rule.condition, context, vals):
+                    vals.update(eval(rule.code))
+                    return vals
+            except ValueError as error:
+                _logger.error(error)
+
+        method = ignore if rule.action == 'delete' else apply
+
+        return list(map(method, vals_list))
+
     def _apply_rules(self, vals_list):
         self.ensure_one()
 
         rules = self._get_rules()
+        _logger.warning(rules)
         if not rules:
             return vals_list
 
-        to_skip, to_delete = [], []
-        context = {
-            'env': self.env,
-            'regex': REGEX_MAJOR_VERSION,
-        }
+        # to_skip, to_delete = [], []
+        context = self._get_eval_context()
 
         for current_rule in rules.get('delete', []):
-            for index, vals in enumerate(vals_list):
-                if eval(current_rule.condition):
-                    _logger.debug("Skip {}".format(vals))
-                    to_skip += [index]
-                    to_delete += [vals]
+            vals_list = self._apply_rule(current_rule, vals_list, context)
 
-        # exclude items
-        vals_list = [vals for index, vals in enumerate(vals_list) if index not in to_skip]
+        # for current_rule in rules.get('delete', []):
+        #     for index, vals in enumerate(vals_list):
+        #         if eval(current_rule.condition, context, vals):
+        #             _logger.debug("Skip {}".format(vals))
+        #             to_skip += [index]
+        #             to_delete += [vals]
 
-        for vals in vals_list:
-            context['vals'] = vals
-            for current_rule in rules.get('update', []):
-                if eval(current_rule.condition, context):
-                    vals.update(eval(current_rule.code, context))
-            for current_rule in rules.get('add_tag', []):
-                if eval(current_rule.condition, context):
-                    vals.update({'tag_ids': [(4, tag.id) for tag in current_rule.tag_ids]})
-            for current_rule in rules.get('add_partner', []):
-                # _logger.debug("Condition : {} = {}".format(current_rule.condition, eval(current_rule.condition, context)))
-                if eval(current_rule.condition, context):
-                    vals.update({'partner_id': current_rule.partner_id.id})
+        # # exclude items
+        # vals_list = [vals for index, vals in enumerate(vals_list) if index not in to_skip]
+
+        # for vals in vals_list:
+        #     # context['vals'] = vals
+        #     for current_rule in rules.get('update', []):
+        #         if eval(current_rule.condition, context, vals):
+        #             vals.update(eval(current_rule.code))
+        #     for current_rule in rules.get('add_tag', []):
+        #         if eval(current_rule.condition, context, vals):
+        #             vals.update({'tag_ids': [(4, tag.id) for tag in current_rule.tag_ids]})
+        #     for current_rule in rules.get('add_partner', []):
+        #         # _logger.debug("Condition : {} = {}".format(current_rule.condition, eval(current_rule.condition, context)))
+        #         if eval(current_rule.condition, context, vals):
+        #             vals.update({'partner_id': current_rule.partner_id.id})
 
         return vals_list
 
@@ -134,141 +179,172 @@ class GitSync(models.AbstractModel):
             record.write({'is_synchronized': not record.is_synchronized})
 
     @api.model
+    def _get_sync_delay(self):
+        # delay = self.env["ir.config_parameter"].sudo().get_param('custom_addons.sync_delay')
+        # return int(delay) if delay else 0
+        return 1000
+
+    def _get_major_version_regex(self):
+        regex = self.env["ir.config_parameter"].sudo().get_param('custom_addons.odoo_major_version')
+        return re.compile(regex)
+
+    @api.model
     def _action_sync(self, ids=[], **kwargs):
 
         cron_mode = kwargs.get('cron', False)
         job_count = kwargs.get('job_count', False)
         force_update = kwargs.get('force_update', False)
+        sync_delay = kwargs.get('delay', self._get_sync_delay())
+        auto_search = kwargs.get('auto_search', False)
 
         records = self.browse(ids)
 
         if cron_mode:
-            param = self.env["ir.config_parameter"].sudo().get_param
-            sync_delay = int(param('custom_addons.sync_delay'))
+            if not records and auto_search:
+                records = self.search([('is_synchronized', '=', True)])
 
-
-            records = self.search([('is_synchronized', '=', True)])
             prev_count = len(records)
             records = records._filter_on_delay(sync_delay)
             _logger.warning("Filter items on delay: {}/{}".format(len(records), prev_count))
 
-            if job_count and len(records) > job_count:
-                records = records[:job_count]
-                _logger.warning("Filter max items: {}/{}".format(len(records), prev_count))
+            # if job_count and len(records) > job_count:
+            #     records = records[:job_count]
+            #     _logger.warning("Filter max items: {}/{}".format(len(records), prev_count))
+
+            if job_count:
+                chunked_ids = [records.ids[i:i+job_count] for i in range(0, len(records.ids), job_count)]
+                for current_ids in chunked_ids:
+                    _logger.error(current_ids)
+                    self._action_sync(current_ids, cron=True, delay=sync_delay)
+                return True
 
 
         _logger.warning("Start action sync on {}: {} items".format(self._description, len(records)))
 
         subtypes = self.env['mail.message.subtype'].search([]).read(['res_model'])
-        subtypes = {item['res_model']:item['id'] for item in subtypes}
-
+        values = kwargs.copy()
+        values['regex'] = self._get_major_version_regex()
+        values['subtypes'] = {item['res_model']:item['id'] for item in subtypes}
 
         for service_name in list(set(records.mapped(self._git_service))):
+            _logger.error(service_name)
 
             records_by_service = records.filtered(lambda rec: rec.service == service_name)
             _logger.warning("Run action sync for {} service on {} items.".format(service_name, len(records_by_service)))
 
             for record in records_by_service:
-                excludes = record._get_excludes()
+                _logger.error('PROCESS {o.id}\t{o.name}'.format(o=record))
+                record._action_process(**values)
 
-                _logger.warning("Excludes: {}".format(excludes))
-
-                # Get items from specific methods
-                items = record._get_items_for_odoo()
-                _logger.warning(items)
-                try:
-                    items = [item for item in items if item.name not in excludes]
-                except AttributeError:
-                    items = [item for item in items if item.get('name') not in excludes]
-
-                # Prepare values, aka convert Git(hub/lab) values to Odoo values
-                vals_list = [record._convert_to_odoo(item) for item in items]
-
-                _logger.debug(vals_list)
-
-                # Apply rules and filter
-                vals_list = record._apply_rules(vals_list)
-
-                _logger.debug(vals_list)
-
-                # Prepare for create or update
-                match_field = record._git_field_name
-                rel_field = record._git_field_rel
-                model_name = record[rel_field]._name
-                model_desc = record[rel_field]._description
-                parent = record[record._git_parent_field] if record._git_parent_field else False
-
-                if record.partner_id and record.force_partner:
-                    vals_list = self._update_list_of_vals(vals_list, {'partner_id': record.partner_id.id})
-
-                # Simple search from current record
-                if record._git_type_rel == 'o2m':
-                    object_ids = {e[match_field]:e['id'] for e in record[rel_field].read([match_field])}
-                    to_update = [(1, object_ids.get(vals[match_field]), vals) for vals in vals_list if vals[match_field] in object_ids.keys()]
-                # More complex, search for all records from rel model
-                elif record._git_type_rel == 'm2m':
-                    object_ids = {e[match_field]:e['id'] for e in self.env[model_name].search([]).read([match_field])}
-                    to_update = [(4, object_ids.get(vals[match_field])) for vals in vals_list if vals[match_field] in object_ids.keys()]
+            # records_by_service.write({'last_sync_date': datetime.now()})
 
 
-                to_create = [(0, False, vals) for vals in vals_list if vals[match_field] not in object_ids]
+    def _action_process(self, force_update=False, **kwargs):
+        excludes = self._get_excludes()
+        subtypes = kwargs.get('subtypes', {})
+        regex = kwargs.get('regex')
 
-                sync_message = "Synchronize {} '{}': {} {} found (updated: {}, created: {})".format(self._name,
-                                                                                     record.name,
-                                                                                     len(vals_list),
-                                                                                     model_name,
-                                                                                     len(to_update),
-                                                                                     len(to_create))
+        _logger.warning("Excludes: {}".format(excludes))
 
+        # Get items from specific methods
+        items = self._get_items_for_odoo()
+        _logger.warning(items)
+        try:
+            items = [item for item in items if item.name not in excludes]
+        except AttributeError:
+            items = [item for item in items if item.get('name') not in excludes]
 
-                _logger.warning(sync_message)
-                record.message_post(body=sync_message, message_type='notification')
+        # Prepare values, aka convert Git(hub/lab) values to Odoo values
+        vals_list = [self._convert_to_odoo(item) for item in items]
 
-                child_ids = record[rel_field]
-                record.update({rel_field: to_update + to_create})
-                new_childs = record[rel_field] - child_ids
-                _logger.error("Childs created: {}".format(new_childs.mapped('name')))
+        _logger.debug(vals_list)
 
-                # [record]                  [childs]            [parent field]
-                # git.organization  -->     git.repository
-                # git.repository    -->     git.branch          organization_id
-                # git.branch        -->     custom.addon        repository_id
+        if self._name != 'git.organization':
+            vals_list = self._check_major_version(vals_list, regex)
 
+        # Apply rules and filter
+        vals_list = self._apply_rules(vals_list)
 
-                parent_name = parent.name if parent else ''
+        _logger.warning(vals_list)
 
-                subtype_id = True
-                if subtype_id and new_childs:
-                    names = ", ".join(new_childs.mapped('name'))
-                    # names = "".join(["<li>{}</li>".format(name) for name in new_childs.mapped('name')])
-                    message = {
-                        'subject': 'GIT',
-                        'body': BODY.format(_(model_desc), record.name, parent_name, names),
-                        'message_type': 'notification',
-                        # 'subtype_id': subtype_id.id
-                    }
-                    if self._name == 'git.branch' and parent:
-                        subtype_id = subtypes.get(parent._name)
-                        if subtype_id:
-                            message['subtype_id'] = subtype_id
-                        parent.message_post(**message)
-                    else:
-                        subtype_id = subtypes.get(record._name)
-                        if subtype_id:
-                            message['subtype_id'] = subtype_id
-                        record.message_post(**message)
+        return True
 
-                if force_update:
-                    to_update = {object_ids.get(vals[match_field]):vals for vals in vals_list if vals[match_field] in object_ids.keys()}
-                    record_ids = self.env[model_name].browse(to_update.keys())
-                    for rec in record_ids:
-                        _logger.warning("Update forced on {} {}".format(model_name, rec.id))
-                        vals = to_update.get(rec.id)
-                        vals['last_sync_date'] = datetime.now()
-                        rec.update(vals)
+        # Prepare for create or update
+        match_field = self._git_field_name
+        rel_field = self._git_field_rel
+        model_name = self[rel_field]._name
+        model_desc = self[rel_field]._description
+        parent = self[self._git_parent_field] if self._git_parent_field else False
+
+        if self.partner_id and self.force_partner:
+            vals_list = self._update_list_of_vals(vals_list, {'partner_id': self.partner_id.id})
+
+        # Simple search from current record
+        if self._git_type_rel == 'o2m':
+            object_ids = {e[match_field]:e['id'] for e in self[rel_field].read([match_field])}
+            to_update = [(1, object_ids.get(vals[match_field]), vals) for vals in vals_list if vals[match_field] in object_ids.keys()]
+        # More complex, search for all records from rel model
+        elif self._git_type_rel == 'm2m':
+            object_ids = {e[match_field]:e['id'] for e in self.env[model_name].search([]).read([match_field])}
+            to_update = [(4, object_ids.get(vals[match_field])) for vals in vals_list if vals[match_field] in object_ids.keys()]
 
 
-            records_by_service.write({'last_sync_date': datetime.now()})
+        to_create = [(0, False, vals) for vals in vals_list if vals[match_field] not in object_ids]
+
+        sync_message = "Synchronize {} '{}': {} {} found (updated: {}, created: {})".format(self._name,
+                                                                                self.name,
+                                                                                len(vals_list),
+                                                                                model_name,
+                                                                                len(to_update),
+                                                                                len(to_create))
+
+
+        _logger.warning(sync_message)
+        self.message_post(body=sync_message, message_type='notification')
+
+        child_ids = self[rel_field]
+        self.update({rel_field: to_update + to_create})
+        new_childs = self[rel_field] - child_ids
+        _logger.error("Childs created: {}".format(new_childs.mapped('name')))
+
+        # [record]                  [childs]            [parent field]
+        # git.organization  -->     git.repository
+        # git.repository    -->     git.branch          organization_id
+        # git.branch        -->     custom.addon        repository_id
+
+
+        parent_name = parent.name if parent else ''
+
+        subtype_id = True
+        if subtype_id and new_childs:
+            names = ", ".join(new_childs.mapped('name'))
+            # names = "".join(["<li>{}</li>".format(name) for name in new_childs.mapped('name')])
+            message = {
+                'subject': 'GIT',
+                'body': BODY.format(_(model_desc), self.name, parent_name, names),
+                'message_type': 'notification',
+                # 'subtype_id': subtype_id.id
+            }
+            if self._name == 'git.branch' and parent:
+                subtype_id = subtypes.get(parent._name)
+                if subtype_id:
+                    message['subtype_id'] = subtype_id
+                parent.message_post(**message)
+            else:
+                subtype_id = subtypes.get(self._name)
+                if subtype_id:
+                    message['subtype_id'] = subtype_id
+                self.message_post(**message)
+
+        if force_update:
+            to_update = {object_ids.get(vals[match_field]):vals for vals in vals_list if vals[match_field] in object_ids.keys()}
+            record_ids = self.env[model_name].browse(to_update.keys())
+            for rec in record_ids:
+                _logger.warning("Update forced on {} {}".format(model_name, rec.id))
+                vals = to_update.get(rec.id)
+                vals['last_sync_date'] = datetime.now()
+                rec.update(vals)
+
 
     def action_sync(self):
         self._action_sync(self.ids, force_update=False)
