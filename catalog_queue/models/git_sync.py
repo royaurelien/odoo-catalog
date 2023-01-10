2938d4310679e846c4d80515b5462370614db92a
Aurelien ROY <roy.aurelien@gmail.com>
2023-01-10 13:52:03 +0100

    [FIX] catalog_queue : batch method

--- catalog_queue/models/git_sync.py
+++ catalog_queue/models/git_sync.py
@@ -29,43 +29,44 @@ class GitSync(models.AbstractModel):
         # values['cron_mode'] = True
         all_records = self.browse(ids)
         # batch = self.env['queue.job.batch'].get_new_batch(all_records._get_batch_name())
-        # batch = self.env['queue.job.batch'].get_new_batch('Group')
+        batch = self.env['queue.job.batch'].get_new_batch(self._description)
         chunk_ids = all_records._group_records_by_identidier(values)
 
         message = f"Synchronizing {self._description}: {chunk_ids[0]}..{chunk_ids[-1]} ({len(chunk_ids)})"
         self.env.user.notify_info(message=message)
 
-        group_a = group(*[self.delayable()._action_sync(current_ids, cron=False) for current_ids in chunk_ids])
-        chain(group_a).delay()
+        # group_a = group(*[self.delayable()._action_sync(current_ids, cron=False) for current_ids in chunk_ids])
+        # chain(group_a).delay()
 
 
 
+        # return True
+
+        for current_ids in chunk_ids:
+            # records = all_records.browse(current_ids)
+
+
+            message = f"Synchronizing {self._description}: {len(current_ids)} item(s)"
+            # res = self.with_context(job_batch=batch).with_delay(channel='catalog')._action_sync(current_ids, **values)
+            # self.with_delay()._action_sync(current_ids, **values)
+            self.with_context(job_batch=batch).with_delay(channel='catalog')._action_sync(current_ids, cron=False)
+            # self._action_sync(current_ids, **values)
+            self.env.user.notify_info(message=message)
+
+            # for record in records:
+
+            #     _logger.warning(record)
+
+            #     # # message = f"Synchronizing {self._description}: {current_ids[0]}..{current_ids[-1]} ({len(current_ids)})"
+            #     message = f"Synchronizing {self._description}: {record.id} ({len(record)})"
+
+            #     self = record.with_context(job_batch=batch)
+            #     self.with_context(job_batch=batch).with_delay(channel='catalog')._action_process(**values)
+            #     self.env.user.notify_info(message=message)
+
+            #     # res = record._action_process(**values)
+            #     # result.append(res)
+
+        batch.enqueue()
         return True
-
-        # for current_ids in chunk_ids:
-        #     # records = all_records.browse(current_ids)
-
-
-        #     message = f"Synchronizing {self._description}: {current_ids[0]}..{current_ids[-1]} ({len(current_ids)})"
-        #     # res = self.with_context(job_batch=batch).with_delay(channel='catalog')._action_sync(current_ids, **values)
-        #     # self.with_delay()._action_sync(current_ids, **values)
-        #     self.with_context(job_batch=batch).with_delay(channel='catalog')._action_sync(current_ids, cron=False)
-        #     # self._action_sync(current_ids, **values)
-        #     self.env.user.notify_info(message=message)
-
-        #     # for record in records:
-
-        #     #     _logger.warning(record)
-
-        #     #     # # message = f"Synchronizing {self._description}: {current_ids[0]}..{current_ids[-1]} ({len(current_ids)})"
-        #     #     message = f"Synchronizing {self._description}: {record.id} ({len(record)})"
-
-        #     #     self = record.with_context(job_batch=batch)
-        #     #     self.with_context(job_batch=batch).with_delay(channel='catalog')._action_process(**values)
-        #     #     self.env.user.notify_info(message=message)
-
-        #     #     # res = record._action_process(**values)
-        #     #     # result.append(res)
-
-        # batch.enqueue()
 
