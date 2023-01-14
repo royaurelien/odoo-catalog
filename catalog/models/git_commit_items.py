4ed9a968b7f3741a6a474438889c84546e863b3a
Aurelien ROY <roy.aurelien@gmail.com>
2023-01-14 14:08:47 +0100

    [IMP] catalog: clean transient

--- catalog/models/git_commit_items.py
+++ catalog/models/git_commit_items.py
@@ -9,32 +9,16 @@ from random import randint
 
 _logger = logging.getLogger(__name__)
 
-RECENT_ACTIVITY_DAYS = 30
-COMMIT_MESSAGE = "{o[last_commit_message]}, {o[last_commit_author]} ({o[last_commit_short_id]})"
-COMMIT_VARS = ['last_commit_message', 'last_commit_author', 'last_commit_short_id', 'last_commit_url', 'last_commit_date']
-
-MESSAGES = {
-    'new_branch': "Found {} from {} repository."
-}
-
-
-# class GitCommitItem(models.TransientModel):
-#     _name = 'git.commit.item'
-#     _description = 'Git Commit Item'
-#     # _order = "last_sync_date desc, name desc"
-
-#     parent_id = fields.Many2one('git.commit.items')
-
-
 
 class GitCommitItems(models.TransientModel):
     _name = 'git.commit.items'
     _description = 'Git Commit Items'
-    # _order = "last_sync_date desc, name desc"
+
+    _transient_max_hours = 1
 
     branch_id = fields.Many2one('git.branch')
     repository_id = fields.Many2one('git.repository')
-    # commit_ids = fields.One2many(comodel_name='git.commit.item', inverse_name='parent_id')
+
     name = fields.Char()
     author = fields.Char()
     email = fields.Char()
@@ -42,6 +26,7 @@ class GitCommitItems(models.TransientModel):
     commit_id = fields.Char()
     commit_url = fields.Char()
     commit_date = fields.Datetime()
+
 
     def action_open_url(self):
         self.ensure_one()
