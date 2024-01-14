666d87c6292c1b753af8e796b14e45f43acc77a0
Aurelien ROY <roy.aurelien@gmail.com>
2024-01-14 15:14:08 +0100

    [WIP]

--- catalog/models/catalog_entry.py
+++ catalog/models/catalog_entry.py
@@ -1,6 +1,7 @@
+import json
 import logging
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -17,7 +18,7 @@ class CatalogEntry(models.Model):
         help="If unchecked, it will allow you to hide the entry without removing it.",
     )
     catalog_tmpl_id = fields.Many2one(
-        "Catalog.template",
+        "catalog.template",
         "Catalog Template",
         auto_join=True,
         index=True,
@@ -31,18 +32,93 @@ class CatalogEntry(models.Model):
     branch = fields.Char(
         index=True,
     )
+
+    manifest = fields.Text()
+
+    version = fields.Char(
+        compute="_compute_manifest",
+        store=True,
+    )
+    license = fields.Char(
+        compute="_compute_manifest",
+        store=True,
+    )
+    author = fields.Char(
+        compute="_compute_manifest",
+        store=True,
+    )
+    website = fields.Char(
+        compute="_compute_manifest",
+        store=True,
+    )
+    category = fields.Char(
+        compute="_compute_manifest",
+        store=True,
+    )
+
+    path = fields.Char(
+        index=True,
+        required=True,
+    )
+    checksum = fields.Char(
+        index=True,
+        required=True,
+    )
     # description = fields.Text()
     # web_description = fields.Html()
     # summary = fields.Char()
     # technical_name = fields.Char(required=True, index=True)
     # application = fields.Boolean(default=False)
     # depends = fields.Char()
-    # active = fields.Boolean(default=True)
     # icon = fields.Char("Icon URL")
     # icon_image = fields.Binary(string="Icon")
     # url = fields.Char()
 
     # author = fields.Char()
 
+    def _get_manifest_fields(self):
+        return ["version", "license", "author", "website", "category"]
+
+    def _get_manifest_values(self):
+        self.ensure_one()
+        manifest = {}
+        fields = self._get_manifest_fields()
+
+        if self.manifest:
+            try:
+                vals = json.loads(self.manifest)
+                manifest = vals.get("manifest", {})
+            except Exception:
+                pass
+        return {field: manifest.get(field, "") for field in fields}
+
+    @api.depends("manifest")
+    def _compute_manifest(self):
+        for record in self:
+            vals = record._get_manifest_values()
+            record.write(vals)
+
     def _compute_is_entry_variant(self):
         self.is_entry_variant = True
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            self.catalog_tmpl_id._sanitize_vals(vals)
+        products = super(
+            CatalogEntry, self.with_context(create_catalog_entry=False)
+        ).create(vals_list)
+        # `_get_variant_id_for_combination` depends on existing variants
+        self.env.registry.clear_cache()
+        return products
+
+    def write(self, values):
+        self.catalog_tmpl_id._sanitize_vals(values)
+        res = super().write(values)
+        if "product_template_attribute_value_ids" in values:
+            # `_get_variant_id_for_combination` depends on `product_template_attribute_value_ids`
+            self.env.registry.clear_cache()
+        elif "active" in values:
+            # `_get_first_possible_variant_id` depends on variants active state
+            self.env.registry.clear_cache()
+        return res
--- catalog/models/catalog_template.py
+++ catalog/models/catalog_template.py
@@ -1,6 +1,7 @@
 import logging
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -11,31 +12,33 @@ class CatalogTemplate(models.Model):
     _description = "Catalog Template"
 
     name = fields.Char(
-        "Name",
         index="trigram",
         required=True,
         translate=True,
     )
+    technical_name = fields.Char(
+        index="trigram",
+        required=True,
+    )
     active = fields.Boolean(
-        "Active",
         default=True,
         help="If unchecked, it will allow you to hide the entry without removing it.",
     )
 
     entry_variant_ids = fields.One2many(
-        "catalog.entry",
-        "catalog_tmpl_id",
-        "Entries",
+        comodel_name="catalog.entry",
+        inverse_name="catalog_tmpl_id",
+        string="Entries",
         required=True,
     )
     # performance: product_variant_id provides prefetching on the first product variant only
     entry_variant_id = fields.Many2one(
-        "catalog.entry",
-        "Entry",
+        comodel_name="catalog.entry",
+        string="Entry",
         compute="_compute_entry_variant_id",
     )
     entry_variant_count = fields.Integer(
-        "# Entry Variants",
+        string="# Entry Variants",
         compute="_compute_entry_variant_count",
     )
     is_entry_variant = fields.Boolean(
@@ -43,11 +46,24 @@ class CatalogTemplate(models.Model):
     )
 
     branch = fields.Char(
-        "Internal Reference",
         compute="_compute_branch",
         inverse="_set_branch",
         store=True,
     )
+
+    manifest = fields.Text(
+        compute="_compute_manifest",
+        inverse="_set_manifest",
+        store=True,
+    )
+
+    _sql_constraints = [
+        (
+            "technical_name_uniq",
+            "unique (technical_name)",
+            "Technical name already exists !",
+        ),
+    ]
 
     @api.depends("entry_variant_ids")
     def _compute_entry_variant_id(self):
@@ -68,6 +84,13 @@ class CatalogTemplate(models.Model):
 
     def _set_branch(self):
         self._set_entry_variant_field("branch")
+
+    @api.depends("entry_variant_ids.manifest")
+    def _compute_manifest(self):
+        self._compute_template_field_from_variant_field("manifest")
+
+    def _set_manifest(self):
+        self._set_entry_variant_field("manifest")
 
     def _compute_template_field_from_variant_field(self, fname, default=False):
         """Sets the value of the given field based on the template variant values
@@ -113,3 +136,93 @@ class CatalogTemplate(models.Model):
                 ).entry_variant_ids
                 if len(archived_variants) == 1:
                     archived_variants[fname] = template[fname]
+
+    def _prepare_variant_values(self, combination=None):
+        self.ensure_one()
+        return {
+            "catalog_tmpl_id": self.id,
+            # 'product_template_attribute_value_ids': [(6, 0, combination.ids)],
+            "active": self.active,
+        }
+
+    def _create_variant_ids(self):
+        if not self:
+            return
+        self.env.flush_all()
+        Entry = self.env["catalog.entry"]
+
+        variants_to_create = []
+        variants_to_activate = Entry
+        variants_to_unlink = Entry
+
+        for tmpl_id in self:
+            # all_variants = tmpl_id.with_context(
+            #     active_test=False
+            # ).entry_variant_ids.sorted(lambda p: (p.active, -p.id))
+
+            variants_to_create.append(tmpl_id._prepare_variant_values())
+
+        if variants_to_activate:
+            variants_to_activate.write({"active": True})
+        if variants_to_create:
+            Entry.create(variants_to_create)
+        if variants_to_unlink:
+            variants_to_unlink._unlink_or_archive()
+            # prevent change if exclusion deleted template by deleting last variant
+            if self.exists() != self:
+                raise UserError(
+                    _("Please archive or delete your product directly if intended.")
+                )
+
+    def _sanitize_vals(self, vals):
+        """Sanitize vales for writing/creating product templates and variants.
+
+        Values need to be sanitized to keep values synchronized, and to be able to preprocess the
+        vals in extensions of create/write.
+        :param vals: create/write values dictionary
+        """
+        if "type" in vals and "detailed_type" not in vals:
+            if vals["type"] not in self.mapped("type"):
+                vals["detailed_type"] = vals["type"]
+        if "detailed_type" in vals and "type" not in vals:
+            type_mapping = self._detailed_type_mapping()
+            vals["type"] = type_mapping.get(
+                vals["detailed_type"], vals["detailed_type"]
+            )
+
+    def _get_related_fields_variant_template(self):
+        """Return a list of fields present on template and variants models and that are related"""
+        return ["branch", "manifest"]
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            self._sanitize_vals(vals)
+        templates = super().create(vals_list)
+        if self._context.get("create_catalog_entry", True):
+            templates._create_variant_ids()
+
+        # This is needed to set given values to first variant after creation
+        for template, vals in zip(templates, vals_list):
+            related_vals = {}
+            for field_name in self._get_related_fields_variant_template():
+                if vals.get(field_name):
+                    related_vals[field_name] = vals[field_name]
+            if related_vals:
+                template.write(related_vals)
+
+        return templates
+
+    def write(self, vals):
+        self._sanitize_vals(vals)
+
+        res = super().write(vals)
+        if self._context.get("create_catalog_entry", True) or (
+            vals.get("active") and len(self.entry_variant_ids) == 0
+        ):
+            self._create_variant_ids()
+        if "active" in vals and not vals.get("active"):
+            self.with_context(active_test=False).mapped("entry_variant_ids").write(
+                {"active": vals.get("active")}
+            )
+        return res
